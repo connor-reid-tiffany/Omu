50c8315dac0ae7e7c89acfe964b47dcf90b619b1
recursive-deletion <30420750+recursive-deletion@users.noreply.github.com>
2017-09-27 11:52:27 -0700

    Update KEGG_Gather.R
    skeleton for KEGG_Gather method

--- Omu/R/KEGG_Gather.R
+++ Omu/R/KEGG_Gather.R
@@ -1,1 +1,10 @@
 #Method for gathering metadata from KEGG API
+
+
+KEGG_Gather <- function(x) UseMethod("KEGG_Gather", x)
+
+KEGG_Gather.cpd <- function(x) "cpd"
+
+KEGG_Gather.rxn <- function(x) "rxn"
+
+KEGG_Gather.genes <- function(x) "KO"
