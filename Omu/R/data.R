0c853e4c92e2353a7bfbae50ffac66b1c46ed90a
Connor Reid Tiffany <connor@Connors-MacBook.local>
2018-06-12 21:51:33 -0700

    "added data documentation"

---
+++ Omu/R/data.R
@@ -0,0 +1,8 @@
+#' c57b6J nos2KO metabolomics count matrix
+#'
+#' A dataset containing metabolomics counts for an experiment done using
+#' c57b6J wild type and c57b6J nos2 knockout mice
+#'
+#' @format A data frame with 668 rows and 36 variables:
+#'
+"c57_nos2KO_mouse_countDF"
